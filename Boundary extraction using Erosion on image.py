b0c4434c194a183827158c906e4fd1141d2ab097
Hima Shankar <121500229+HimaMaddala@users.noreply.github.com>
2024-06-09 11:58:59 +0530

    Create Boundary extraction using Erosion on image.py

---
+++ Boundary extraction using Erosion on image.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+img=cv2.imread('your_path\\apple.jpg',0)
+kernel=np.ones((3,3),np.uint8)
+
+img_erosion=cv2.erode(img,kernel,iterations=1)
+boundary_extracted=img-img_erosion
+
+cv2.imshow('Input',img)
+cv2.imshow('Erosion',img_erosion)
+cv2.imshow('Boundary ',boundary_extracted)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
