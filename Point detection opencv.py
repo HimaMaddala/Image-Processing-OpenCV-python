3eb654451a89a7eeebcb6ff2d88652c202fc723a
Hima Shankar <121500229+HimaMaddala@users.noreply.github.com>
2024-06-20 15:04:35 +0530

    Update Point detection opencv.py

--- Point detection opencv.py
+++ Point detection opencv.py
@@ -4,12 +4,10 @@ import numpy as np
 image = cv2.imread("image.jpg", cv2.IMREAD_GRAYSCALE)
 
 blurred = cv2.GaussianBlur(image, (3,3), 0)
-
 laplacian = cv2.Laplacian(blurred, cv2.CV_64F)
 
 log = cv2.Laplacian(blurred, cv2.CV_64F)
 log =np.uint8(np.absolute(log))
-
 _, points = cv2.threshold(log, 30, 255, cv2.THRESH_BINARY)
 
 cv2.imshow('Original image', image)
